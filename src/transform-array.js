25d138b64116642f5f82983ea247799e3e8bd7c5
TimaLebed <timalebedevskiy@mail.ru>
2020-03-05 17:54:28 +0300

    feat: add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,29 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+
+    const transformed = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                i++;
+                break;
+            case '--discard-prev':
+                transformed.pop();
+                break;    
+            case '--double-next':
+                i + 1 !== arr.length ? transformed.push(arr[i + 1]) : null;
+                break;
+            case '--double-prev':
+                i - 1 > 0 ? transformed.push(arr[i - 1]) : null;
+                break;
+            default:
+                transformed.push(arr[i]);
+                break;
+        }
+    }
+
+    return transformed;
 };
