e520f01af05796a4376c1f4c134314413e60ce49
TimaLebed <timalebedevskiy@mail.ru>
2020-03-09 20:01:50 +0300

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,50 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(isDirect = true) {
+      this.isDirect = isDirect;
+      this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
-
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  
+    encrypt(msg, key) {
+      this.checkError(msg, key);
+  
+      return this.algorithm(msg.toUpperCase(), key.toUpperCase());
     }
-}
-
-module.exports = VigenereCipheringMachine;
+  
+    decrypt(msg, key) {
+      this.checkError(msg, key);
+  
+      return this.algorithm(msg.toUpperCase(), key.toUpperCase(), true);
+    }
+  
+    checkError(msg, key) {
+      if (!msg || !key) {
+        throw Error;
+      }
+    }
+  
+    algorithm(msg, key, isDecrypt = false) {
+      const { alphabet, isDirect } = this;
+      const alphabetLength = alphabet.length;
+      const result = [];
+  
+      for (let i = 0; i < msg.length; i++) {
+        const currentChar = msg.charAt(i);
+        const indexedChar = alphabet.indexOf(key.charAt(i % key.length));
+        const correctIndexChar = isDecrypt ?
+          alphabet.indexOf(currentChar) - indexedChar + alphabetLength :
+          alphabet.indexOf(currentChar) + indexedChar;
+  
+        if (alphabet.includes(currentChar)) {
+          result.push(alphabet.charAt(correctIndexChar % alphabetLength));
+        } else {
+          result.push(currentChar);
+          msg = msg.slice(0, i) + msg.slice(i + 1);
+          i--;
+        }
+      }
+  
+      return isDirect ? result.join('') : result.reverse().join('');
+    }
+  }
+  
+  module.exports = VigenereCipheringMachine;
